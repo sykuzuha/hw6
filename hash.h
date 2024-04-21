2f648c4050584ab485e1016e1c9137e264556635
Athena Sy Kuzuhara <sykuzuha@usc.edu>
2024-04-21 06:06:17 +0000

    finished hash function

--- hash.h
+++ hash.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <random>
 #include <chrono>
+//#include <iostream>
 
 typedef std::size_t HASH_INDEX_T;
 
@@ -20,7 +21,39 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+        std::string str = k;
+        for (std::string::size_type i=0; i<k.length(); ++i)
+        {
+            str[i] = std::tolower(k[i]);
+        }
+        unsigned long long w[5] = {0,0,0,0,0};
+        int stringIndex = str.length() - 1;
+        int wIndex = 4;
+        int power[] = {1, 36, 1296, 46656, 1679616, 60466176};
+        int powerIndex = 0;
+        while((stringIndex >= 0) && (wIndex >= 0))
+        {
+            w[wIndex] += (power[powerIndex]*letterDigitToNumber(str[stringIndex]));
+            stringIndex-=1;
+            powerIndex+=1;
+            if(powerIndex > 5)
+            {
+              powerIndex = 0;
+              wIndex-=1;
+            }
+            //std::cout << "stringIndex: " << stringIndex << std::endl;
+        
 
+
+        }
+        HASH_INDEX_T result = 0;
+        for(int i = 0; i <= 4; i++)
+        {
+            //std::cout << "w[" << i << "] = " << w[i] << std::endl;
+            result += rValues[i]*w[i];
+
+        }
+        return result;
 
     }
 
@@ -28,6 +61,19 @@ struct MyStringHash {
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
+        HASH_INDEX_T num;
+        if((letter >= 48) && (letter <= 57))
+        {
+          //0 is 48 in ASCII, 9 is 57 in ASCII
+          num = letter - 22;
+        }
+        else
+        {
+          //a is 97 in ASCII
+          num = letter - 97;
+        }
+        //std::cout << letter << " turns to: " << num << std::endl;
+        return num;
 
     }
 
